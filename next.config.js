fb38eaa7f32a7e25216785c53f48b6b0593e70de
Xavier2p <xavier.de-place@epita.fr>
2023-10-20 11:13:21 +0200

    new project started

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  assetPrefix: isProd ? 'Xavier2p.github.io.' : '',
-  images: {
-    unoptimized: true,
-  },
-};
--- src/utils/bin/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './about';
-export * from './projects';
-export * from './social';
-export * from './utils';
-export * from './weather';
-export * from './neofetch';
-export * from './cowsay';
