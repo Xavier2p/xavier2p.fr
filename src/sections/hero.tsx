0d2fef71f7def5c8a54682f7332a6b7a4288e953
Xavier2p <xavier.de-place@epita.fr>
2024-02-02 00:26:13 +0100

    feat: add company

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,4 +1,4 @@
-import { Alert, Code, Text, Title } from '@mantine/core';
+import { Alert, Anchor, Text, Title } from '@mantine/core';
 import cnf from '../config';
 import SocialGroup from '../components/social';
 import { IconAlertTriangle } from '@tabler/icons-react';
@@ -12,7 +12,8 @@ const Hero = () => (
       Homelaber, NixOS fanboy, Tech enthusiast and maybe Hacker
     </Text>
     <Text size="lg" mt="xs" mb="lg">
-      I'm a studying at EPITA and I work as DevOps at <Code>COMPANY_NAME</Code>.
+      I'm a studying at EPITA and I work as IT Manager at{' '}
+      <Anchor href="https://cartesian-lab.com">Cartesian Lab</Anchor>.
     </Text>
     <SocialGroup socials={cnf.socials} />
     <Alert variant="light" color="yellow" radius="md" title="Warning" icon={<IconAlertTriangle />}>
